ed5cfcd68697bea1f75da5bc9df16bd3ab1ef497
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-01-27 12:26:15 +0530

    Updated `Learn React` to `Access Materials`

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Access Materials
         </a>
       </header>
     </div>
