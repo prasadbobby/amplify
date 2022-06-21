2b54e5e6c9b70ecb8bccbe00b2855b99336f01e4
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-06-21 11:04:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prasad Bobby -- EDU Materials.
+          Prasad Bobby -- EDU Materials
         </p>
         <a
           className="App-link"
