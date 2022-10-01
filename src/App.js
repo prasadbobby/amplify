11250987d613b17815a8621b0d1b709bb8bd48bd
mahendra gandham <92915886+mahendrasaikumargandham@users.noreply.github.com>
2022-10-01 20:33:55 +0530

    Updated `App.js` 🚀

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Prasad Bobby -- EDU Materials.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/prasadbobby/edu-materials"
-        >
-          Access Materials
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
